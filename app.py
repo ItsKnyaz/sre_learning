4c98354560cf4b848ff87a3ac9902d2d8d3a28c7
ItsKnyaz <KnazevSergey.0412@gmail.com>
2026-09-09 21:09:54 +0300

    Intial app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+def calculate(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
+
+def main():
+    print(calculate(2, 3))
+    print(multiply(4, 5))
+
+if __name__ == "__main__":
+    main()
