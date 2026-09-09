67b56f11012ea23ad0f5531fe865f73e5a892951
ItsKnyaz <KnazevSergey.0412@gmail.com>
2026-09-09 21:16:05 +0300

    Update app.py in main

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 def calculate(x, y):
-    return x + y
+    return x + y + 100  # Изменено по-другому
 
 def multiply(x, y):
-    return x * y
+    return x * y + 100  # Изменено по-другому
 
 def main():
     print(calculate(2, 3))
     print(multiply(4, 5))
+    print("Main update")  # Добавлено по-другому
 
 if __name__ == "__main__":
     main()
